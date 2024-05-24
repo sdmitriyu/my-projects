a80e7f2b8becfb9708cc45dd0f509e8e689a2c43
Дмитрий Соловьёв <sdmitriyyu@gmail.com>
2024-05-24 12:59:22 +0300

    Заработали деньги на период.

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from days import Days
 from many_day import Many_of_day
-from periodMany import Many_of_period
 
-days = Days
-many_day = Many_of_day
+# Создаем экземпляры классов
+days_instance = Days()
 
-many_day.return_many_day() = days.period_days * many_day.retManyDay
-result = periodMany.many_of_period.period_many
+money_per_day = input('Сколько денег в день вы планируете потратить? ')
+try:
+    money_per_day = float(money_per_day)  # Преобразуем ввод пользователя в число
+except ValueError:
+    print("Пожалуйста, введите число")
+
+many_day = Many_of_day(money_per_day)
+
+# Получаем результаты от методов классов
+days_period_days, end_date = days_instance.days_of_period()
+many_day_result = many_day.return_many_day()
+
+# Вычисляем итоговый результат
+result = days_period_days * many_day_result
 print(result)
