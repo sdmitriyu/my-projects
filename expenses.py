919fbc8df6b1a48354db447f527c626ce9faf124
Дмитрий Соловьёв <sdmitriyyu@gmail.com>
2024-05-24 12:57:10 +0300

    аработали деньги на период.

--- expenses.py
+++ expenses.py
@@ -1,6 +1,10 @@
+from datetime import datetime
+
 class Expenses:
     def __init__(self, expenses):
         self.expenses = expenses
-
+        self.daynow = datetime.date()
     def ret(self):
         return self.expenses(int(input()))
+
+    def
